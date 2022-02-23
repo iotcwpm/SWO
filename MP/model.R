1ba24cf014b749e18b90b9e8f9915ff428ca66a8
Iago Mosqueira <iago@mosqueira.eu>
2022-02-23 08:15:29 +0100

    Tuning runs

---
+++ MP/model.R
@@ -0,0 +1,46 @@
+# model.R - DESC
+# /model.R
+
+# Copyright Iago MOSQUEIRA (WMR), 2021
+# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+
+library(mse)
+
+source("utilities.R")
+
+# progressr
+
+#library(progressr)
+#handlers(global=TRUE)
+
+#library(doFuture)
+#registerDoFuture()
+#plan(multicore, workers=25)
+
+library(doParallel)
+registerDoParallel(2)
+
+# DATA
+load("data/om.Rdata")
+
+# DEBUG
+refpts(om)$Ftarget <- refpts(om)$FMSY 
+refpts(om)$SBlim <- refpts(om)$SBMSY * 0.20
+
+# MSE arguments
+
+mseargs <- list(iy=2019, data_lag=2, frq=3)
+
+# METRICS
+
+mets <- list(Rec=function(x) unitSums(rec(x)), SB=function(x) unitSums(ssb(x)),
+  C=function(x) unitSums(catch(x)), F=function(x) unitMeans(fbar(x)))
+
+# RELATIVE metrics
+
+relmets <- list(SBMSY=function(x) unitSums(ssb(x)) %/% refpts(om)$SBMSY,
+  SB0=function(x) unitSums(ssb(x)) %/% refpts(om)$SB0,
+  FMSY=function(x) unitMeans(fbar(x)) %/% refpts(om)$FMSY) 
